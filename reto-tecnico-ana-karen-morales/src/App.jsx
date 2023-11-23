18e0ee92bac5ae88ab81f2e77d35e55f629ae3c4
AKMoralesRamos <dental.morak@gmail.com>
2023-11-23 13:18:04 -0600

    Creación de interfaz con título, input y botón creados con bootstrap

--- reto-tecnico-ana-karen-morales/src/App.jsx
+++ reto-tecnico-ana-karen-morales/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
+  
+ /*  handleCalcular = () => {
 
-  return (
-    <>
+  } */
+
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Calculadora de serie</h1>
+        <p>Introduce un valor para 'n' y haz click en 'Calcular'.</p>
+        <div>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Ingresa un número"
+            />
+            <Button variant="dark" id="button-addon2"/*  onClick={handleCalcular} */>
+              Calcular
+            </Button>
+          </InputGroup>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
