44920e26c29637222a59484f5c6bcfcd7098965c
AKMoralesRamos <dental.morak@gmail.com>
2023-11-25 04:14:44 -0600

    creación de clase para método, agregar imagen de la función

--- reto-tecnico-ana-karen-morales/src/components/result.jsx
+++ reto-tecnico-ana-karen-morales/src/components/result.jsx
@@ -1,22 +1,19 @@
-import React from "react";
 import Modal from "react-bootstrap/Modal";
 
-class ResultModal extends React.Component {
-  render() {
-    const { result, show, handleClose } = this.props;
-    console.log(result);
-
-    return (
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Resultado</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>El valor de la posición de "n" en la serie es: {result}</p>
-        </Modal.Body>
-      </Modal>
-    );
-  }
+function ResultModal({ result, show, handleCloseModal, numberValue }) {
+  return (
+    <Modal show={show} onHide={handleCloseModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Resultado</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          El valor de la posición de "n = {numberValue}" en la serie es:{" "}
+          {result}
+        </p>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default ResultModal;
